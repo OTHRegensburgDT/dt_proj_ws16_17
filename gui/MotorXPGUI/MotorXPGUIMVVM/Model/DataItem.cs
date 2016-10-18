0acebff487a5928dbe8202ce740164f2f8d153a7
Ricardo K <kaser23@gmx.de>
2016-10-18 16:30:42 +0200

    REMOVE: MotorXPGUI project ADD: MotorXPGUIMVVM project

---
+++ gui/MotorXPGUI/MotorXPGUIMVVM/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace MotorXPGUIMVVM.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
