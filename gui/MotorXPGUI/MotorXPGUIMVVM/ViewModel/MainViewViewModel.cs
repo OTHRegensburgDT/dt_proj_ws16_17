cc706d1e3ea41239ae9c67412e5d58c4fc510ec5
Ricardo K. <kaser23@gmx.de>
2016-10-18 16:30:42 +0200

    REMOVE: MotorXPGUI project ADD: MotorXPGUIMVVM project

---
+++ gui/MotorXPGUI/MotorXPGUIMVVM/ViewModel/MainViewViewModel.cs
@@ -0,0 +1,40 @@
+﻿using GalaSoft.MvvmLight;
+using MotorXPGUIMVVM.Model;
+
+namespace MotorXPGUIMVVM.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewViewModel : ViewModelBase
+    {
+        private readonly IDataService _dataService;
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewViewModel(IDataService dataService)
+        {
+            _dataService = dataService;
+            _dataService.GetData(
+                (item, error) =>
+                {
+                    if (error != null)
+                    {
+                        // Report error here
+                        return;
+                    }
+                });
+        }
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
