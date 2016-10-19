9c932997135f4cb5a4cc7c3eac8093cf5b442d48
Michael Schleinkofer <michael.schleinkofer@st.oth-regensburg.de>
2016-10-19 20:18:54 +0200

    Initial commit; Suggestion for Sensordata object;

---
+++ com/PC_Part/KomModule/KomModule/RegulationParams.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomModule
+{
+    class RegulationParams
+    {
+        private UInt64 timestamp;
+        private KeyValuePair<UInt16, UInt64> paramTable;
+
+        public KeyValuePair<UInt16, UInt64> ParamTable
+        {
+            get { return paramTable; }
+            set { paramTable = value; }
+        }
+
+        public UInt64 Timestamp
+        {
+            get { return timestamp; }
+            set { timestamp = value; }
+        }
+    }
+}
---
+++ com/PC_Part/KomModule/KomModule/Sensordata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KomModule
+{
+    public class Sensordata
+    {
+        private UInt64 timestamp;
+        private SortedList<UInt16, UInt64> dataTable;
+
+        public UInt64 Timestamp
+        {
+            get { return timestamp; }
+            set { timestamp = value; }
+        }
+
+        public SortedList<UInt16, UInt64> DataTable
+        {
+            get { return dataTable; }
+            set { dataTable = value; }
+        }
+    }
+}
