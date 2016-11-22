679091a335a74294bde114bfaa4940b248e801bd
Ricardo K. <kaser23@gmx.de>
2016-11-22 13:57:30 +0100

    ADD: Usercontrol to display data

---
+++ gui/MotorXPGUI/MotorXPGUIMVVM/ViewModel/DataDisplay.cs
@@ -0,0 +1,24 @@
+﻿using MotorXPGUIMVVM.Controls.Gauge;
+using MotorXPGUIMVVM.Controls.LineChart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorXPGUIMVVM.ViewModel
+{
+    public class DataDisplayVM
+    {
+        private Gauge _gauge;
+        private LineChart _line;
+        public DataDisplayVM()
+        {
+            _line = new LineChart();
+            _line.AddValue(42);
+        }
+
+        
+
+    }
+}
