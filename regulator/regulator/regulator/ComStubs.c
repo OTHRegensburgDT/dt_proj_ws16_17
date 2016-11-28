9fc01b82f7ac07af35e59926bd57ea0d14b74339
BerndNK <BerndKrupinski@gmail.com>
2016-11-28 19:32:21 +0100

    - Add header files from communication to project structure - Create stubs for communication methods - Adjust ComHandler.h to include new files

---
+++ regulator/regulator/regulator/ComStubs.c
@@ -0,0 +1,26 @@
+#include "ComHandler.h"
+
+// communication.h
+bool initCom(void) { return true; }
+
+bool sendSensorData(Sensordata* data) { return true; }
+
+bool processRegulationData() { return true; }
+
+void DataRcvICR() {}
+
+// FrameParser.h
+extern bool FrameToProto(uint8_t* buffer, int* size) { return true; }
+
+
+extern bool ProtoToFrame(uint8_t* buffer, int* size) { return true; }
+
+// Paramsparser.h
+extern bool ProtoToParams(uint8_t* protoMsg, int size) { return true; }
+
+extern bool ParamsToProto(uint8_t* protoMsg, int* size) { return true; }
+
+// Sensorparser.h
+extern bool ProtoToSensor(Sensordata* outData, uint8_t* protoMsg, int size) { return true; }
+
+extern bool SensorToProto(uint8_t* protoMsg, int* size, Sensordata* inData) { return true; }
