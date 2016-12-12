02fbd32a215e58f6e583f6478035bcc02950a0b8
RK-003 <RK-003@DESKTOP-TKLPLIV>
2016-12-12 19:17:46 +0100

    CHANGE cleanup code

--- gui/MotorXPGUI/MotorXPGUIMVVM/Model/SensorDataCollection.cs
+++ gui/MotorXPGUI/MotorXPGUIMVVM/Model/SensorDataCollection.cs
@@ -1,30 +1,152 @@
-﻿using MotorXPGUIMVVM.Annotations;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using MotorXPGUIMVVM.Annotations;
 
 namespace MotorXPGUIMVVM.Model
 {
     public sealed class SensorDataCollection : INotifyPropertyChanged
     {
-        private BindingList<double> _values;
-        private double _lastValue;
+        private ulong _currentSample;
         private double _currentValue;
-        private bool _showAll = true;
         private bool _expanderCollapsed;
+        private double _lastValue;
+        private ulong _sampleWindow = 10;
+        private bool _showAll = true;
         private int _targetValue;
         private int _trashValue;
-        private ulong _currentSample;
-        private ulong _sampleWindow = 10;
+        private BindingList<double> _values;
 
         public SensorDataCollection(SensorDataType type)
         {
             SensorDataType = type;
             Unit = type.ToString();
             InitSensorSettings();
-
         }
+
+
+        public ulong LastTimeStamp { get; set; }
+
+        public ulong CurrentSample
+        {
+            get { return _currentSample; }
+            set
+            {
+                _currentSample = value;
+                OnPropertyChanged(nameof(CurrentSample));
+            }
+        }
+
+        public ulong SampleWindow
+        {
+            get { return _sampleWindow; }
+            set
+            {
+                if (value > LastTimeStamp)
+                    _sampleWindow = LastTimeStamp;
+                else if (value <= 10)
+                    _sampleWindow = 10;
+                _sampleWindow = value;
+                OnPropertyChanged(nameof(SampleWindow));
+            }
+        }
+
+        public string Unit { get; }
+
+        public bool ExpanderCollapsed
+        {
+            get { return _expanderCollapsed; }
+            set
+            {
+                _expanderCollapsed = value;
+                OnPropertyChanged(nameof(ExpanderCollapsed));
+            }
+        }
+
+        public double CurrentValue
+        {
+            get { return _showAll ? _lastValue : _currentValue; }
+            set
+            {
+                _currentValue = value;
+                OnPropertyChanged(nameof(CurrentValue));
+                OnPropertyChanged(nameof(CurrentValueText));
+            }
+        }
+
+        public string CurrentValueText
+            => $"Current Value: {(_showAll ? _lastValue.ToString("00") : _currentValue.ToString("00"))}";
+
+        public int TargetValue
+        {
+            get { return _targetValue; }
+            set
+            {
+                var newValue = value;
+                if (int.TryParse(newValue.ToString(), out _trashValue))
+                {
+                    if ((newValue >= MinValue) && (newValue <= MaxValue))
+                    {
+                        _targetValue = newValue;
+                        OnPropertyChanged(nameof(TargetValue));
+                    }
+                }
+                else
+                {
+                    OnPropertyChanged(nameof(TargetValue));
+                }
+            }
+        }
+
+        public bool ShowAll
+        {
+            get { return _showAll; }
+            set
+            {
+                _showAll = value;
+                if (_showAll)
+                    SampleWindow = LastTimeStamp;
+                OnPropertyChanged(nameof(ShowAll));
+            }
+        }
+
+        public double LastValue
+        {
+            get { return _lastValue; }
+            set
+            {
+                if (_showAll)
+                    _currentValue = value;
+                _lastValue = value;
+                OnPropertyChanged(nameof(LastValue));
+            }
+        }
+
+        public int MinValue { get; private set; }
+
+        public int MaxValue { get; private set; }
+
+        public SensorDataType SensorDataType { get; }
+
+        public BindingList<double> Values
+        {
+            get { return _values; }
+            private set
+            {
+                _values = value;
+                LastValue = _values.LastOrDefault();
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ShowAllCommand { get; set; }
+
+        public int GaugeTickFrequency => MaxValue/10;
+
+        public bool HasTargetValue { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
 
         private void InitSensorSettings()
         {
@@ -56,140 +178,6 @@ namespace MotorXPGUIMVVM.Model
                     break;
             }
         }
-
-
-        public ulong LastTimeStamp { get; set; }
-
-        public ulong CurrentSample
-        {
-            get { return _currentSample; }
-            set
-            {
-                _currentSample = value;
-                OnPropertyChanged(nameof(CurrentSample));
-            }
-        }
-
-        public ulong SampleWindow
-        {
-            get { return _sampleWindow; }
-            set
-            {
-                if (value > LastTimeStamp)
-                {
-                    _sampleWindow = LastTimeStamp;
-                }
-                else if (value <= 10)
-                {
-                    _sampleWindow = 10;
-                }
-                _sampleWindow = value;
-                OnPropertyChanged(nameof(SampleWindow));
-            }
-        }
-
-        public string Unit { get; }
-
-        public bool ExpanderCollapsed
-        {
-            get { return _expanderCollapsed; }
-            set
-            {
-                _expanderCollapsed = value;
-                OnPropertyChanged(nameof(ExpanderCollapsed));
-            }
-        }
-
-        public double CurrentValue
-        {
-            get
-            {
-                return _showAll ? _lastValue : _currentValue;
-            }
-            set
-            {
-                _currentValue = value;
-                OnPropertyChanged(nameof(CurrentValue));
-                OnPropertyChanged(nameof(CurrentValueText));
-            }
-        }
-
-        public string CurrentValueText => $"Current Value: {(_showAll ? _lastValue.ToString("00") : _currentValue.ToString("00"))}";
-
-        public int TargetValue
-        {
-            get { return _targetValue; }
-            set
-            {
-                var newValue = value;
-                if (int.TryParse(newValue.ToString(), out _trashValue))
-                {
-                    if (newValue >= MinValue && newValue <= MaxValue)
-                    {
-
-                        _targetValue = newValue;
-                        OnPropertyChanged(nameof(TargetValue));
-                    }
-                }
-                else
-                {
-                    OnPropertyChanged(nameof(TargetValue));
-                }
-            }
-        }
-
-        public bool ShowAll
-        {
-            get { return _showAll; }
-            set
-            {
-                _showAll = value;
-                if (_showAll)
-                {
-                    SampleWindow = LastTimeStamp;
-                }
-                OnPropertyChanged(nameof(ShowAll));
-            }
-        }
-
-        public double LastValue
-        {
-            get { return _lastValue; }
-            set
-            {
-                if (_showAll)
-                {
-                    _currentValue = value;
-                }
-                _lastValue = value;
-                OnPropertyChanged(nameof(LastValue));
-            }
-        }
-
-        public int MinValue { get; private set; }
-
-        public int MaxValue { get; private set; }
-
-        public SensorDataType SensorDataType { get; }
-
-        public BindingList<double> Values
-        {
-            get { return _values; }
-            private set
-            {
-                _values = value;
-                LastValue = _values.LastOrDefault();
-                OnPropertyChanged();
-            }
-        }
-
-        public ICommand ShowAllCommand { get; set; }
-
-        public int GaugeTickFrequency => MaxValue/10;
-
-        public bool HasTargetValue { get; set; }
-
-        public event PropertyChangedEventHandler PropertyChanged;
 
         [NotifyPropertyChangedInvocator]
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
