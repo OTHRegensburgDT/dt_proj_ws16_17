fd4312f68fe484787b4711038ba43b719311f77d
RK-003 <RK-003@DESKTOP-TKLPLIV>
2016-12-21 15:49:10 +0100

    ADD comments

--- gui/MotorXPGUI/MotorXPGUIMVVM/Model/SensorDataCollection.cs
+++ gui/MotorXPGUI/MotorXPGUIMVVM/Model/SensorDataCollection.cs
@@ -18,6 +18,10 @@ namespace MotorXPGUIMVVM.Model
         private BindingList<double> _values;
         private ulong _lastTimeStamp;
 
+        /// <summary>
+        /// ctor set the sensorDataType for a collection and run some init methods 
+        /// </summary>
+        /// <param name="type">sensordatatype</param>
         public SensorDataCollection(SensorDataType type)
         {
             SensorDataType = type;
@@ -25,7 +29,9 @@ namespace MotorXPGUIMVVM.Model
             InitSensorSettings();
         }
 
-
+        /// <summary>
+        /// get or set the latest displayed timestamp  
+        /// </summary>
         public ulong LastTimeStamp
         {
             get { return _lastTimeStamp; }
@@ -48,6 +54,9 @@ namespace MotorXPGUIMVVM.Model
             }
         }
 
+        /// <summary>
+        /// this is the current samplewindow which is displayed in the gui
+        /// </summary>
         public ulong SampleWindow
         {
             get { return _sampleWindow; }
@@ -69,8 +78,14 @@ namespace MotorXPGUIMVVM.Model
             }
         }
 
+        /// <summary>
+        /// get the unit as string from a sensordata collection
+        /// </summary>
         public string Unit { get; }
 
+        /// <summary>
+        /// get or set the current value from a sensorvalue, if show all is selected this should be everytime the last value from values
+        /// </summary>
         public double CurrentValue
         {
             get { return _showAll ? _lastValue : _currentValue; }
@@ -82,22 +97,29 @@ namespace MotorXPGUIMVVM.Model
             }
         }
 
+        /// <summary>
+        /// check the min max bounds for a value
+        /// </summary>
+        /// <param name="value"> the incomming value</param>
+        /// <returns>the value in bounds</returns>
         private double CheckMinMax(double value)
         {
             if (value <= MinValue)
             {
                 return MinValue;
             }
-            if (value >= MaxValue)
-            {
-                return MaxValue;
-            }
-            return value;
+            return value >= MaxValue ? MaxValue : value;
         }
 
+        /// <summary>
+        /// get the current value as text string
+        /// </summary>
         public string CurrentValueText
             => $"Current Value: {(_showAll ? _lastValue.ToString("00") : _currentValue.ToString("00"))}";
 
+        /// <summary>
+        /// this property is only used for the velocety get get and set a target value
+        /// </summary>
         public int TargetValue
         {
             get { return _targetValue; }
@@ -133,6 +155,9 @@ namespace MotorXPGUIMVVM.Model
             }
         }
 
+        /// <summary>
+        /// get or set the last value from the value list and set the current value to the last if show all is selected
+        /// </summary>
         public double LastValue
         {
             get { return _lastValue; }
@@ -147,12 +172,35 @@ namespace MotorXPGUIMVVM.Model
             }
         }
 
+        /// <summary>
+        /// this is the minimum gauge angle value to configure the radius from the gauge
+        /// </summary>
+        public double MinGaugeAngle { get; private set; }
+
+        /// <summary>
+        /// this is the maximum gauge angle value to configure the radius from the gauge
+        /// </summary>
+        public double MaxGaugeAngle { get; private set; }
+
+        /// <summary>
+        /// this is the minimum value which can be reached for a unit
+        /// </summary>
         public int MinValue { get; private set; }
 
+        /// <summary>
+        /// this is the maximum value which can be reached for a unit
+        /// </summary>
         public int MaxValue { get; private set; }
 
+        /// <summary>
+        /// the unit type of a data object
+        /// </summary>
         public SensorDataType SensorDataType { get; }
 
+        /// <summary>
+        /// add a value to the value list and update the gui and the last value
+        /// </summary>
+        /// <param name="value">new value as double</param>
         public void AddValue(double value)
         {
             _values.Add(value);
@@ -162,7 +210,8 @@ namespace MotorXPGUIMVVM.Model
         }
 
         /// <summary>
-        /// all recived values should be a ringbuffer 
+        /// all recived values 
+        /// ToDo change to a ringbuffer and persist overflowing data  
         /// </summary>
         public BindingList<double> Values
         {
@@ -179,17 +228,35 @@ namespace MotorXPGUIMVVM.Model
             }
         }
 
+        /// <summary>
+        /// command triggers if showAll check changed
+        /// </summary>
         public ICommand ShowAllCommand { get; set; }
 
+        /// <summary>
+        /// this slider start the send params method if the user release the mouse from the target slider
+        /// </summary>
         public ICommand SliderMouseButtonUpCommand { get; set; }
 
+        /// <summary>
+        /// indicates if a sensordatacollection can have a target value, at the moment only the velocity possible
+        /// </summary>
         public bool HasTargetValue { get; set; }
 
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        /// initialize the sensordatacollection for a specific sensordata type
+        /// </summary>
         private void InitSensorSettings()
         {
             Values = new BindingList<double>();
+            MinGaugeAngle = 4.65;
+            MaxGaugeAngle = 7.85;
+            //ToDo put settings like this in a config file
             switch (SensorDataType)
             {
                 case SensorDataType.Velocity:
@@ -201,6 +268,7 @@ namespace MotorXPGUIMVVM.Model
                 case SensorDataType.Angle:
                     MinValue = 0;
                     MaxValue = 359;
+                    MaxGaugeAngle = 10.934;
                     break;
                 case SensorDataType.Temp:
                     MinValue = 0;
