02978b33f8425828b076107fb610c9d30431c8c4
RK-003 <RK-003@DESKTOP-TKLPLIV>
2016-12-21 15:54:50 +0100

    REMOVE unused using

--- gui/MotorXPGUI/MotorXPGUIMVVM/Repository/MockCommunicator.cs
+++ gui/MotorXPGUI/MotorXPGUIMVVM/Repository/MockCommunicator.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Navigation;
 using KomModule;
 
 namespace MotorXPGUIMVVM.Repository
